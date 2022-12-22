629d3cab406853daee6df6ca47027b906693de34
Federico Jure <fjure@atixlabs.com>
2022-12-22 16:49:41 -0300

    component Carousel and Statistics

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import { ChakraProvider } from "@chakra-ui/react";
+import Carousel from "./components/Carousel";
+import BasicStatistics from "./components/Statistics";
 
 function App() {
   return (
     <div className="App">
       <ChakraProvider>
         <Navbar />
+        <Carousel />
+        <BasicStatistics />
       </ChakraProvider>
     </div>
   );
