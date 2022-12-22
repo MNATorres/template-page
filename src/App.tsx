875b52208679c08d2fab99cc6d1690c10921656e
Federico Jure <fjure@atixlabs.com>
2022-12-22 16:27:02 -0300

    componente Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from "react";
+import Navbar from "./components/Navbar";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
   return (
     <div className="App">
-      <p>Hola</p>
+      <ChakraProvider>
+        <Navbar />
+      </ChakraProvider>
     </div>
   );
 }
